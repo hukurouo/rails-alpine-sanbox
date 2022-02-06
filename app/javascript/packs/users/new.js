2a25d35446b9f82ae4bc35af34082d9ba55188a3
hukurouo <owlbook248@gmail.com>
2022-02-06 17:01:11 +0900

    検索可能、複数選択可能な選択フォーム

--- app/javascript/packs/users/new.js
+++ app/javascript/packs/users/new.js
@@ -4,6 +4,16 @@ import 'select2'
 $(function () {
   $('.js-searchable').select2({
     dropdownAutoWidth: true,
+    placeholder: "検索語句を入力",
+    allowClear: true,
     theme: 'bootstrap4'
+  });
+  
+  $('.js-searchable-multiple').select2({
+    dropdownAutoWidth: true,
+    theme: 'bootstrap4',
+    placeholder: "検索語句を入力",
+    allowClear: true,
+    multiple: true
   });
 });
